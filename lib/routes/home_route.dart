e55a3eb0ee5011da17db3e818f2783904e963f07
Siddharth Rao <siddrao.926@gmail.com>
2024-09-18 02:37:44 -0700

    Added Hertz Display

--- lib/routes/home_route.dart
+++ lib/routes/home_route.dart
@@ -6,26 +6,37 @@ class HomeRoute extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Padding(
-        padding: EdgeInsets.symmetric(
+        padding: const EdgeInsets.symmetric(
           horizontal: 16,
         ),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(
-              "A",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 175,
-                fontWeight: FontWeight.bold,
-              ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "A",
+                  style: TextStyle(
+                    fontSize: 175,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  "440 Hz",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.white.withOpacity(0.5),
+                  ),
+                ),
+              ],
             ),
-            SizedBox(height: 16),
-            TuningIndicator(
-              value: 0.0,
+            const TuningIndicator(
+              cents: 30,
             ),
           ],
         ),
